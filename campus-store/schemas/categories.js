1c0dab9dcd9763c2188b56daabc94e1026491b1c
georgeankumensah <georgeankumensah@gmail.com>
2022-05-02 00:18:44 +0000

    added schemas

---
+++ campus-store/schemas/categories.js
@@ -0,0 +1,22 @@
+export default {
+    name : "categories",
+    title : "Categories",
+    type:"document",
+    fields:[
+        {
+            name:"category",
+            title:"Category",
+            type:"string",
+        },
+        {
+            name : "image",
+            title : "Image",
+            type : "image",
+            options:{
+                hotspot:true,
+
+            }
+
+        },
+    ]
+}
